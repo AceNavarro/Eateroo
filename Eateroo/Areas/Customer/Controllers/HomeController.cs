8dde8078b28546e115d0422e9b779fb67dd1c3db
Ace Navarro <pogs_acenavarro@yahoo.com>
2019-03-28 00:29:31 -0700

    Add landing page for customer

--- Eateroo/Areas/Customer/Controllers/HomeController.cs
+++ Eateroo/Areas/Customer/Controllers/HomeController.cs
@@ -5,15 +5,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Eateroo.Models;
+using Eateroo.Models.ViewModels;
+using Eateroo.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Eateroo.Controllers.Customer
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
-        [Area("Customer")]
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = new IndexViewModel()
+            {
+                MenuItems = await _db.MenuItem.Include(m => m.Category)
+                                              .Include(m => m.SubCategory)
+                                              .ToListAsync(),
+                Categories = await _db.Category.ToListAsync(),
+                Coupons = await _db.Coupon.Where(c => c.IsActive).ToListAsync()
+            };
+
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
